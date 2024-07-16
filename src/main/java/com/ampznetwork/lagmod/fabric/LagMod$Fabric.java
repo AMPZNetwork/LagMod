d6daf218dcf6456c81eebb973317b9095d9babf1
Kaley <kaleidox@comroid.org>
2024-07-16 14:20:37 +0200

    basic implementation

--- src/main/java/com/ampznetwork/lagmod/fabric/LagMod$Fabric.java
+++ src/main/java/com/ampznetwork/lagmod/fabric/LagMod$Fabric.java
@@ -1,29 +1,108 @@
 package com.ampznetwork.lagmod.fabric;
 
+import com.mojang.brigadier.ParseResults;
+import com.mojang.brigadier.context.CommandContextBuilder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.kyori.adventure.text.Component;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.command.CommandManager;
+import net.minecraft.server.command.CommandOutput;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.text.Text;
+import net.minecraft.util.math.Vec2f;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.NotNull;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
 
-public class LagMod$Fabric implements ModInitializer, ServerLifecycleEvents.ServerStarted {
-    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(2);
-    private       ScheduledFuture<?>       task;
+import static net.kyori.adventure.text.serializer.gson.GsonComponentSerializer.*;
+
+@Getter
+@NoArgsConstructor
+@Slf4j(topic = LagMod$Fabric.AddonName)
+public class LagMod$Fabric implements ModInitializer, ServerLifecycleEvents.ServerStarted, ServerLifecycleEvents.ServerStopping {
+    public static final String AddonName = "LagMod";
+
+    public static Text component2text(Component component) {
+        return Text.Serializer.fromJson(gson().serialize(component));
+    }
+
+    private final     ScheduledExecutorService     scheduler = Executors.newScheduledThreadPool(2);
+    private final     Set<StandaloneCleanupCycler> cyclers   = new HashSet<>();
+    private @NonFinal MinecraftServer              server;
 
     @Override
     public void onInitialize() {
         ServerLifecycleEvents.SERVER_STARTED.register(this);
+        ServerLifecycleEvents.SERVER_STOPPING.register(this);
     }
 
     @Override
     public void onServerStarted(MinecraftServer server) {
-        task = scheduler.scheduleAtFixedRate(this::cleanupEntities, 15, 15, TimeUnit.MINUTES);
+        this.server = server;
+
+        for (var world : server.getWorlds()) {
+            var cycler = new StandaloneCleanupCycler(this, world);
+            cyclers.add(cycler);
+        }
     }
 
-    private void cleanupEntities() {
-        
+    @Override
+    public void onServerStopping(MinecraftServer server) {
+        scheduler.shutdown();
+    }
+
+    public void executeCommand(World world, String command) {
+        var mgr = server.getCommandManager();
+        var out = new CommandOutput() {
+            @Override
+            public void sendMessage(Text message) {
+                log.info(message.getString());
+            }
+
+            @Override
+            public boolean shouldReceiveFeedback() {
+                return false;
+            }
+
+            @Override
+            public boolean shouldTrackOutput() {
+                return false;
+            }
+
+            @Override
+            public boolean shouldBroadcastConsoleToOps() {
+                return false;
+            }
+        };
+        var parse = getParseResults((ServerWorld) world, out, mgr);
+        mgr.execute(parse, command);
+    }
+
+    private @NotNull ParseResults<ServerCommandSource> getParseResults(ServerWorld world, CommandOutput out, CommandManager mgr) {
+        var src = new ServerCommandSource(out,
+                Vec3d.ZERO,
+                Vec2f.ZERO,
+                world,
+                0,
+                LagMod$Fabric.AddonName,
+                Text.of(LagMod$Fabric.AddonName),
+                server,
+                null);
+        return new ParseResults<>(new CommandContextBuilder<>(
+                mgr.getDispatcher(),
+                src,
+                mgr.getDispatcher().getRoot(),
+                0));
     }
 }
---
+++ src/main/java/com/ampznetwork/lagmod/fabric/StandaloneCleanupCycler.java
@@ -0,0 +1,55 @@
+package com.ampznetwork.lagmod.fabric;
+
+import lombok.Value;
+import lombok.experimental.NonFinal;
+import lombok.extern.slf4j.Slf4j;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minecraft.world.World;
+
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import static com.ampznetwork.lagmod.fabric.LagMod$Fabric.*;
+import static net.kyori.adventure.text.Component.*;
+import static net.kyori.adventure.text.format.NamedTextColor.*;
+
+@Value
+@Slf4j(topic = LagMod$Fabric.AddonName)
+public class StandaloneCleanupCycler {
+    LagMod$Fabric mod;
+    World         world;
+    @NonFinal ScheduledFuture<?> recurring;
+
+    public StandaloneCleanupCycler(LagMod$Fabric mod, World world) {
+        this.mod       = mod;
+        this.world     = world;
+        this.recurring = mod.getScheduler().scheduleAtFixedRate(this::startCleanupCycle, 1, 30, TimeUnit.MINUTES);
+    }
+
+    private void startCleanupCycle() {
+        mod.getScheduler().schedule(this::cycleWarn1, 0, TimeUnit.SECONDS);
+        mod.getScheduler().schedule(this::cycleWarn2, 30, TimeUnit.SECONDS);
+        mod.getScheduler().schedule(this::cleanupEntities, 1, TimeUnit.MINUTES);
+    }
+
+    public void cycleWarn1() {
+        mod.getServer().getPlayerManager().broadcast(component2text(warningText("1 minute")), false);
+    }
+
+    private Component warningText(String remaining) {
+        return text()
+                .append(text("Warning: ").color(GOLD))
+                // dont ask me why the fuck i have to qualify the class in the next call
+                .append(text("All dropped items will be removed in " + remaining).color(NamedTextColor.YELLOW))
+                .build();
+    }
+
+    public void cycleWarn2() {
+        mod.getServer().getPlayerManager().broadcast(component2text(warningText("30 seconds")), false);
+    }
+
+    private void cleanupEntities() {
+        mod.executeCommand(world, "execute in %s run kill @e[type=item]".formatted(world.getRegistryKey().toString()));
+    }
+}
